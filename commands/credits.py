fc11cb1eef20efaeaed9483debb3a9b5152a1b45
Harry <38505228+katznboyz1@users.noreply.github.com>
2019-04-15 10:27:06 -0400

    Added the date to the credits command

--- commands/credits.py
+++ commands/credits.py
@@ -1,3 +1,3 @@
 print ('Credits:')
-print ('Made by katznboyz/katznboyz1')
+print ('Made by katznboyz/katznboyz1 (April-2019)')
 print ('Yeah thats all the credits I did it by myself because I have no friends.')
