8ef7c1b44315794619160e4c423431449741316d
Harry <38505228+katznboyz1@users.noreply.github.com>
2019-04-15 10:43:33 -0400

    Fixed the limit parameter and fixed the output's formatting.

--- commands/getRedditPostLinks.py
+++ commands/getRedditPostLinks.py
@@ -1,6 +1,6 @@
 #make it so that it can get posts after a certain amount
 
-import argparse, urllib.request, json
+import argparse, urllib.request, json, urllib
 
 parser = argparse.ArgumentParser(description = 'A program that will get a list of reddit posts on a certain page.')
 
@@ -18,21 +18,34 @@ after = 0
 if (args.after):
     after = args.after
 
-redditData = urllib.request.urlopen('https://www.reddit.com/r/{}/.json?limit={}&after={}'.format(subreddit, limit, after))
+try:
+    redditData = urllib.request.urlopen('https://www.reddit.com/r/{}/.json?limit={}&after={}'.format(subreddit, limit, after))
+except urllib.error.HTTPError as err:
+    print ('429 HTTP Error, try waiting for a little bit and then using this command again.')
+    exit()
+
 redditData = json.loads(redditData.read().decode())
 
 redditData = redditData['data']['children']
 
 print ('-------------------------------------------------------------------------------')
 
-print ('Showing {} posts from r/{}:'.format(limit, subreddit))
+print ('Showing {} posts from r/{}:'.format(str(int(limit) + 1), subreddit))
 
-for post in range(len(redditData)):
+for post in range(int(limit + 1)):
     title = redditData[post]['data']['title']
-    url = redditData[post]['data']['url']
+    url = redditData[post]['data']['permalink']
     upvotes = redditData[post]['data']['score']
+    comments = redditData[post]['data']['num_comments']
+    uploader = redditData[post]['data']['author']
     try:
-        print ('\n{} - {}\n{} upvotes'.format(title, url, str(upvotes)))
+        print ('''
+Title: {}
+URL: https://www.reddit.com{}
+Upvotes: {}
+Comments: {}
+Uploader: u/{}
+'''.format(title, url, upvotes, comments, uploader))
     except:
         print ('\nError printing this title. It probably had an emoji in it.')
 
