3ed4ffc3d0987d24453397c5773d0f01b1d21b30
Harry <38505228+katznboyz1@users.noreply.github.com>
2019-04-15 17:18:42 -0400

    Added a new required module.
    Added "colorama" to the required modules.

--- installRequiredModules.py
+++ installRequiredModules.py
@@ -1,7 +1,7 @@
 #python script made for autoinstalling all the required packages for the reddit browser
 import os
 pythonPath = str(open('./manifest.txt').read()).split('\n')[0].split('=')[1]
-requiredModules = ['pillow', 'argparse', 'numpy']
+requiredModules = ['pillow', 'argparse', 'numpy', 'colorama']
 for each in requiredModules:
     os.system('{} -m pip install {}'.format(pythonPath, each))
 print ('Done installing the required modules for the reddit browser.')
