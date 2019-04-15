2747b1caab8d447e3c4e5ad54dba2ec93811eed9
Harry <38505228+katznboyz1@users.noreply.github.com>
2019-04-15 17:23:01 -0400

    Updated a few things and removed a requirement.

--- installRequiredModules.py
+++ installRequiredModules.py
@@ -1,7 +1,7 @@
 #python script made for autoinstalling all the required packages for the reddit browser
 import os
 pythonPath = str(open('./manifest.txt').read()).split('\n')[0].split('=')[1]
-requiredModules = ['pillow', 'argparse', 'numpy', 'colorama']
+requiredModules = ['pillow', 'argparse', 'numpy']
 for each in requiredModules:
     os.system('{} -m pip install {}'.format(pythonPath, each))
 print ('Done installing the required modules for the reddit browser.')
--- python-cli-reddit-browser.py
+++ python-cli-reddit-browser.py
@@ -1,4 +1,4 @@
-import os
+import os, colorama
 os.chdir('./commands') #set the directory to the commands folder
 pythonName = str(open('../manifest.txt').read()).split('\n')[0].split('=')[1]
 print ('Type "help.py" for a full list of commands and other help info.')
