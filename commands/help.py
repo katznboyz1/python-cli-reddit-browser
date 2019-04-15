319b30eb3f0dda1ba32087a41ee84cd456b93fef
Harry <38505228+katznboyz1@users.noreply.github.com>
2019-04-15 10:23:53 -0400

    Added new sentence to the help command.

--- commands/help.py
+++ commands/help.py
@@ -1,6 +1,7 @@
 print ('Python CLI Reddit Browser Command Menu:\n')
-print ('All commands must end with the proper file extension or else they will not run. For example, to run the help command, you can do "./help.py" or "help.py".\n')
+print ('All commands must end with the proper file extension or else they will not run. For example, to run the help command, you can do "help.py".\n')
 print ('Commands:\n')
 print ('help.py - Prints a list of commands for the browser.')
+print ('credits.py - Prints the credits for this project.')
 print ('getImageFromUrl.py - Prints the image out in the command line. This function isnt fully functional and will be improved on later.')
 print ('getRedditPostLinks.py - Returns a list of reddit post links from a specific page.')
