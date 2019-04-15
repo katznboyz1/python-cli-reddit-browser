f792dfde4b7a026115448a4eb9f1cfce984c60c7
Harry <38505228+katznboyz1@users.noreply.github.com>
2019-04-15 17:27:35 -0400

    Re uploaded everything

--- commands/getImageFromUrl.py
+++ commands/getImageFromUrl.py
@@ -13,6 +13,9 @@ scale = 1
 if (args.scale):
     scale = args.scale
 
+if ('cache' not in os.listdir('..')):
+    os.mkdir('../cache')
+
 pythonName = str(open('../manifest.txt').read()).split('\n')[0].split('=')[1]
 
 imageFilePath = '../cache/tmpImage1.' + args.url.split('.')[-1]
@@ -22,3 +25,6 @@ urllib.request.urlretrieve(url, imageFilePath)
 os.system('{} imageToText.py --file {} --scale {} --out ../cache/asciiImageOutput.txt >> ../cache/out.txt'.format(pythonName, imageFilePath, scale))
 
 print (str(open('../cache/asciiImageOutput.txt').read()))
+
+os.remove(imageFilePath)
+os.remove('../cache/asciiImageOutput.txt')
