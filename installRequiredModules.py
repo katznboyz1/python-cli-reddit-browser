f653ef937e8e9b8f9404b742e2af7600c64b166d
Harry <38505228+katznboyz1@users.noreply.github.com>
2019-04-15 14:25:40 -0400

    Fixed a small detail
    Instead of using an input to determine the python installation name, it just reads it from the manifest file.

--- installRequiredModules.py
+++ installRequiredModules.py
@@ -1,6 +1,6 @@
 #python script made for autoinstalling all the required packages for the reddit browser
 import os
-pythonPath = str(input('Python Name (python, py.exe, python3 [...]): '))
+pythonPath = str(open('./manifest.txt').read()).split('\n')[0].split('=')[1]
 requiredModules = ['pillow', 'argparse', 'numpy']
 for each in requiredModules:
     os.system('{} -m pip install {}'.format(pythonPath, each))
